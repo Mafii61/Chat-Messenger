46308dc5c208ccb3486df6dcff98429583f47982
Mafii61 <simih6161@gmail.com>
2024-09-08 13:37:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello, World!"
+def start_page():
+    return "<h1>Hello, World!</h1><a href='https://www.google.de/'>Google</a>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
